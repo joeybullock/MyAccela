b8d3f382e0fb0c293172c74f98fcafe5acd73e11
Joey Bullock <35963215+joeybullock@users.noreply.github.com>
2018-09-20 17:50:20 -0400

    added send email script for revision reqd status on QCR's

--- Atlanta/Scripts/Event/WTUA/WTUA;Building!QCR!~!~.js
+++ Atlanta/Scripts/Event/WTUA/WTUA;Building!QCR!~!~.js
@@ -1,2 +1,3 @@
 true ^ showDebug = false; showMessage = false;
 wfTask == "Close" && wfStatus == "Closed" ^ sendQCR_ZONING_APPROVAL();
+wfStatus.indexOf("App Notified") > -1 ^ sendNotificationSimple("QCR_APPL_NOTIFIED_REVISIONS_REQD","Y");
