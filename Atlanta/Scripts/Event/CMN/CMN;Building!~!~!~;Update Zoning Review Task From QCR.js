dbbf2b06d01fc939c2de2dc00bafa0f92f1a6232
Joey Bullock <35963215+joeybullock@users.noreply.github.com>
2018-09-24 11:59:37 -0400

    commented out functions still in dev

--- Atlanta/Scripts/Event/CMN/CMN;Building!~!~!~;Update Zoning Review Task From QCR.js
+++ Atlanta/Scripts/Event/CMN/CMN;Building!~!~!~;Update Zoning Review Task From QCR.js
@@ -1,4 +1,4 @@
 true ^ showDebug = false; showMessage = false;
-true ^ parentId = new Array(); parentId = getParents("Building/QCR/*/*"); holdId = capId; capId = parentId[0];
+//true ^ parentId = new Array(); parentId = getParents("Building/QCR/*/*"); holdId = capId; capId = parentId[0];
 //capId && taskStatus("Close") ^ var zoningTaskComments = getTaskStatus("Zoning Inspection", "N", "N", "Y"); capId = holdId; updateTask("Zoning Review","Approved","Approved from QCR Review:" + "\n" + zoningTaskComments,""); deactivateTask("Plan Coordination");
 //capId && taskStatus("Close") ^ copyTaskResultsToAnotherTask(capId,"Zoning Inspection",holdId,"Zoning Review");
