14b9ca6071861e3dcb0aaad2b1ba7bdf4c05eb2d
Joey Bullock <35963215+joeybullock@users.noreply.github.com>
2018-09-24 15:02:58 -0400

    commit

--- Atlanta/Scripts/Event/CMN/CMN;Building!~!~!~;Inspection Prior To Other Inspections.js
+++ Atlanta/Scripts/Event/CMN/CMN;Building!~!~!~;Inspection Prior To Other Inspections.js
@@ -1,2 +1,2 @@
 showMessage = false; showDebug = false;
-inspType == "190 Building Final" && (isTaskStatus("Arborist Review","Approved") || isTaskStatus("Arborist Final Review","Approved")) && !checkInspectionResult("590 Arborist Final","Passed") ^ message+="<br>Cannot schedule a Building Final until it has passed an Arborist Final inspection."; showMessage = true; cancel = true;
+inspType == "190 Building Final" && (isTaskStatus("Arborist Review","Approved") || isTaskStatus("Arborist Final Approval","Approved")) && !checkInspectionResult("590 Arborist Final","Passed") ^ message+="<br>Cannot schedule a Building Final until it has passed an Arborist Final inspection."; showMessage = true; cancel = true;
